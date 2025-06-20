21c5bce6f9f8a6c9e86198ef8ce2c27cb9522db5
Joshelee <a1923678@adelaide.edu.au>
2025-06-20 03:44:43 +0000

    [automatic commit] 2025-06-20 03:44:43 UTC ;; communications error to 208.67.222.222#53: timed out 129.127.249.163

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -37,12 +37,12 @@ router.get('/api/walkrequests/open', async (req, res) => {
           wr.requested_time,
           wr.duration_minutes,
           wr.location,
-          u.username       AS owner_username
-        FROM WalkRequests wr
-        JOIN Dogs d   ON wr.dog_id   = d.dog_id
-        JOIN Users u  ON d.owner_id  = u.user_id
-        WHERE wr.status = 'open'
-      `);
+        u.username       AS owner_username
+      FROM WalkRequests wr
+      JOIN Dogs d   ON wr.dog_id   = d.dog_id
+      JOIN Users u  ON d.owner_id  = u.user_id
+      WHERE wr.status = 'open'
+    `);
 
     return res.json(rows);
   } catch (err) {
