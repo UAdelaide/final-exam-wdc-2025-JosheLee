87272053ccb42cd2a735959a9bcaefeab120de40
Joshelee <a1923678@adelaide.edu.au>
2025-06-20 03:43:37 +0000

    [automatic commit] 2025-06-20 03:43:37 UTC 129.127.249.163

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -31,7 +31,17 @@ router.get('/api/dogs', async (req, res) => {
 router.get('/api/walkrequests/open', async (req, res) => {
   try {
     const [rows] = await db.query(`
-      SELECT 
+      SELECT
+        wr.request_id,
+        d.name           AS dog_name,
+        wr.requested_time,
+        wr.duration_minutes,
+        wr.location,
+        u.username       AS owner_username
+      FROM WalkRequests wr
+      JOIN Dogs d   ON wr.dog_id   = d.dog_id
+      JOIN Users u  ON d.owner_id  = u.user_id
+      WHERE wr.status = 'open'
       `);
   } catch (err) {
     return res.status(500).json({error: 'Database error'});
