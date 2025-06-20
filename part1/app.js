d4cfc25a69589ef00490a1e0d079c7e9502539cf
Joshelee <a1923678@adelaide.edu.au>
2025-06-20 04:18:54 +0000

    [automatic commit] 2025-06-20 04:18:54 UTC 129.127.249.163

--- part1/app.js
+++ part1/app.js
@@ -25,6 +25,7 @@ app.use(cookieParser());
         await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
         await connection.end();
 
+        
 })();
 
 app.use(express.static(path.join(__dirname, 'public')));
