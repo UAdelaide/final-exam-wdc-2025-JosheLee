7b1327c82d7bab602cd0205c258b74fee5476d88
Joshelee <a1923678@adelaide.edu.au>
2025-06-20 04:15:49 +0000

    [automatic commit] 2025-06-20 04:15:49 UTC 129.127.249.163

--- part1/app.js
+++ part1/app.js
@@ -17,8 +17,9 @@ app.use(cookieParser());
 (async () => {
     try {
         const connection = await mysql.createConnection({
-            host
+            host: 'localhost'
         });
+        
     }
 })();
 
