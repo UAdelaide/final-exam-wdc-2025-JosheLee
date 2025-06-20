89e134e51e30086e58923fb5ce8459d4830a32df
Joshelee <a1923678@adelaide.edu.au>
2025-06-20 03:45:24 +0000

    [automatic commit] 2025-06-20 03:45:24 UTC 129.127.249.163

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -12,9 +12,9 @@ router.get('/api/dogs', async (req, res) => {
   try {
     const [rows] = await db.query(`
       SELECT
-        d.name      AS dog_name,
-        d.size      AS size,
-        u.username  AS owner_username
+        d.name AS dog_name,
+        d.size AS size,
+        u.username AS owner_username
       FROM Dogs d
       JOIN Users u ON d.owner_id = u.user_id
     `);
@@ -37,7 +37,7 @@ router.get('/api/walkrequests/open', async (req, res) => {
         wr.requested_time,
         wr.duration_minutes,
         wr.location,
-        u.username       AS owner_username
+        u.username AS owner_username
       FROM WalkRequests wr
       JOIN Dogs d   ON wr.dog_id   = d.dog_id
       JOIN Users u  ON d.owner_id  = u.user_id
