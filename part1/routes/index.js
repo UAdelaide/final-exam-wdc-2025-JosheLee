5cb977cecbdb3a440a5bd65ffaec35898f1ffcdf
Joshelee <a1923678@adelaide.edu.au>
2025-06-20 03:49:10 +0000

    [automatic commit] 2025-06-20 03:49:10 UTC 129.127.249.163

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -52,7 +52,9 @@ router.get('/api/walkrequests/open', async (req, res) => {
 
 router.get('/api/walkers/summary', async (req, res) => {
   try {
+    const [rows] = await db.query(
 
+    )
   } catch (err) {
     return res.status(500).json({ error: 'Database error' });
   }
