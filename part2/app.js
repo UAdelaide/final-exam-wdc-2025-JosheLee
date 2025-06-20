551c8228fc601140e842e2bab2ebe294bc94c4a4
Joshelee <a1923678@adelaide.edu.au>
2025-06-20 07:24:01 +0000

    [automatic commit] 2025-06-20 07:24:01 UTC 129.127.249.163

--- part2/app.js
+++ part2/app.js
@@ -12,7 +12,13 @@ app.use(express.static(path.join(__dirname, '/public')));
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
-
+// Session
+app.use(session({
+    secret: 'doggies',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, maxAge: 1000 * 60 * 60 * 24 }
+}));
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
